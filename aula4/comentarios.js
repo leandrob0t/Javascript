4098deadbc9e3af4d9873efd905310a57ad07065
leandrob0t <125820074+leandrob0t@users.noreply.github.com>
2023-04-22 15:02:18 -0300

    aula4

---
+++ aula4/comentarios.js
@@ -0,0 +1,12 @@
+//Escrever um comentário (Comentário de Linha)
+console.log('Olá, Mundo!!'); //Aqui tem outro comentário
+console.log('Linha 1');
+console.log('Linha 2');
+//Comentário
+console.log('Linha 3');
+console.log('Linha 4');
+/*
+Comentário em bloco, Mesmo utilizado no css3.
+*/ 
+console.log('Linha 5');
+//Outro comentário
