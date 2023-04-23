945d983eb4f3ec1c0ecdce65b468d95352a9f446
leandrob0t <125820074+leandrob0t@users.noreply.github.com>
2023-04-23 13:50:44 -0300

    att6

---
+++ aula6/index.js
@@ -0,0 +1,11 @@
+let nome = 'Leo';
+/*Não pode redeclarar variável let, ex: let nome = 'Marcos';
+Pode modificar o valor da variável sem utilizar a palavra let, como foi feito abaixo.;
+NÃO UTILIZE VAR, SEMPRE UTILIZE LET.*/
+console.log(nome, 'nasceu em 1984.');
+console.log('Em 2000', nome, 'conheceu Maria.');
+nome = 'Pedro';
+console.log(nome, 'casou com Maria em 2012.');
+console.log('Maria teve um filho com', nome, 'em 2015.');
+nome = 'José'
+console.log('O filho de', nome, 'se chama Eduardo.');
