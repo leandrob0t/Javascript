14313d333e281cf18c9f2d7d931bdf058a7d89b9
leandrob0t <125820074+leandrob0t@users.noreply.github.com>
2023-04-23 14:37:55 -0300

    att7

---
+++ aula7/index.js
@@ -0,0 +1,8 @@
+//NÃO PODE MODIFICAR O VALOR DE UMA CONST
+const primeiroNumero= 5; //number
+const segundoNumero= 10;
+const resultado = primeiroNumero * segundoNumero;
+const resultadoDuplicado = resultado * 2;
+let resultadoTriplicado = resultado * 3;
+resultadoTriplicado = resultadoTriplicado + 5;
+console.log(typeof primeiroNumero);
