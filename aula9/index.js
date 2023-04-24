0c089ee3b9cd591fdc005e8732144d341eb6d2a7
leandrob0t <125820074+leandrob0t@users.noreply.github.com>
2023-04-24 13:27:26 -0300

    att9

---
+++ aula9/index.js
@@ -0,0 +1,11 @@
+//Dados primitivos, string, number, undefined, null, boolean, symbol.
+const nome = 'Leo'; //string
+const nome1 = "Leo";//string
+const nome2 = `Leo`;//string
+const num1 = 10;//number
+const num2 = 10.50;//number
+let nomeAluno; //undefined = não aponta para local nenhum na memória.
+const sobrenomeAluno = null; //nulo = não aponta para local nenhum na memória.
+const aprovado = false; // Boolean = true, false (lógico)
+
+console.log(typeof num2, num2)
