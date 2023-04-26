14ac024716d28782ce8df266a38f3aeb09154bc3
leandrob0t <125820074+leandrob0t@users.noreply.github.com>
2023-04-26 13:34:08 -0300

    att11

---
+++ aula11/js/index.js
@@ -0,0 +1,10 @@
+let num1 = prompt('Digite um número:');
+let num2 = prompt('Digite outro número:');
+
+ num1 = Number(num1);
+ num2 = Number(num2);
+//let resultado = num1 + num2;
+//alert(`O resultado é ${resultado}`);
+//Maneira mais prática para código simples.
+alert(`O resultado foi ${num1 + num2}.`);
+
